e550f20820cbc65fe63011a5b3c4194de19c15f1
DalyChouikh <chouikhdaly215@gmail.com>
2023-05-04 00:51:03 +0100

    Added DELETE PUT to EventController

--- src/main/java/dev/daly/issatsoevents/controller/EventController.java
+++ src/main/java/dev/daly/issatsoevents/controller/EventController.java
@@ -5,6 +5,8 @@ import dev.daly.issatsoevents.entity.ImageData;
 import dev.daly.issatsoevents.service.EventService;
 import dev.daly.issatsoevents.service.ImageService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -42,6 +44,46 @@ public class EventController {
     @GetMapping
     public ResponseEntity<List<Event>> getAllEvents(){
         return ResponseEntity.ok(eventService.getAllEvents());
+    }
+
+    @GetMapping("{id}/image")
+    public ResponseEntity<?> getEventImage(@PathVariable Long id){
+        return ResponseEntity.status(HttpStatus.OK)
+                .contentType(MediaType.valueOf("image/png"))
+                .body(eventService.getEventImage(id));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteEvent(@PathVariable Long id){
+        if (eventService.getEvent(id) == null)
+            return ResponseEntity.badRequest().body("Event with id " + id + " not found");
+        eventService.deleteEvent(id);
+        return ResponseEntity.ok("Event with id " + id + " deleted successfully");
+    }
+
+    @DeleteMapping()
+    public ResponseEntity<?> deleteAllEvent(){
+        eventService.deleteAllEvent();
+        return ResponseEntity.ok("Events deleted successfully");
+    }
+
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Event> updateEvent(@PathVariable Long id,
+                                             @RequestParam("name") String name,
+                                             @RequestParam("description") String description,
+                                             @RequestParam("location") String location,
+                                             @RequestParam("date") LocalDate date,
+                                             @RequestParam("time") LocalTime time,
+                                             @RequestParam("organizer") String organizer,
+                                             @RequestParam("image") MultipartFile image) throws IOException {
+        Event event = eventService.getEvent(id);
+        ImageData imageData =  imageService.uploadImage(image);
+        if (event == null)
+            return ResponseEntity.badRequest().body(null);
+
+
+        return ResponseEntity.ok(eventService.updateEvent(event, name, description, location, date, time, organizer, imageData));
     }
 
 
--- src/main/java/dev/daly/issatsoevents/service/EventService.java
+++ src/main/java/dev/daly/issatsoevents/service/EventService.java
@@ -3,6 +3,7 @@ package dev.daly.issatsoevents.service;
 import dev.daly.issatsoevents.entity.Event;
 import dev.daly.issatsoevents.entity.ImageData;
 import dev.daly.issatsoevents.repository.EventRepository;
+import dev.daly.issatsoevents.utils.ImageUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -47,4 +48,40 @@ public class EventService {
     public List<Event> getAllEvents() {
         return eventRepository.findAll();
     }
+
+    public byte[] getEventImage(Long id) {
+        ImageData dbImageData =  eventRepository.findById(id).get().getImageData();
+        byte[] images= ImageUtils.decompressImage(dbImageData.getImageData());
+        return images;
+    }
+
+    public void deleteEvent(Long id) {
+        eventRepository.deleteById(id);
+    }
+
+    public Event updateEvent(Event event,
+                            String name,
+                            String description,
+                            String location,
+                            LocalDate date,
+                            LocalTime time,
+                            String organizer,
+                            ImageData imageData) throws IOException {
+        event.setName(name);
+        event.setDescription(description);
+        event.setLocation(location);
+        event.setDate(date);
+        event.setTime(time);
+        event.setOrganizer(organizer);
+        event.setImageData(imageData);
+        eventRepository.save(event);
+        return event;
+    }
+
+    public void deleteAllEvent() {
+        eventRepository.deleteAll();
+    }
+
+
+
 }
