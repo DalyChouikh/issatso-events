3661b6e72c98762dc53a3addccb3ca15d5b8a01b
DalyChouikh <chouikhdaly215@gmail.com>
2023-05-03 20:04:08 +0100

    Initial commit

---
+++ src/main/java/dev/daly/issatsoevents/IssatsoEventsApplication.java
@@ -0,0 +1,13 @@
+package dev.daly.issatsoevents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IssatsoEventsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IssatsoEventsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/daly/issatsoevents/IssatsoEventsApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.daly.issatsoevents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IssatsoEventsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
